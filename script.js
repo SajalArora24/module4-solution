ba652135616fd408d5c1f3b5f0c66371d5669e1a
SajalArora24 <110257155+SajalArora24@users.noreply.github.com>
2023-05-09 14:43:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function () {
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = ["Yaakov", "Jack", "Jene", "Jordan", "Paul", "Henry", "Luorl", "Paula", "Laura", "Jimmy"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
